c6969b825f2ed626eaefe05d42bd0c96fa01511f
migueel15 <miguelangeldorado10@gmail.com>
2026-05-23 18:50:25 +0200

    refactor main

--- cmd/senda/main.go
+++ cmd/senda/main.go
@@ -8,15 +8,6 @@ import (
 )
 
 func main() {
-	// pos := os.Args[1]
-	// image := os.Args[2]
-	// cmd := exec.Command("awww", "img", "--transition-bezier", ".61,.18,.48,.9", "--transition-duration", "1", "--transition-fps", "144", "--transition-type", "grow", "--transition-pos", pos, image)
-	//
-	// cmd.Run()
-	//
-	// cmd = exec.Command("notify-send", "Wallpaper updated!")
-	// cmd.Run()
-
 	backend := awww.NewBackend()
 	service := wallpaper.NewService(backend)
 
