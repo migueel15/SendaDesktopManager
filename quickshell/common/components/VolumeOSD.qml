e39c754fe98e2a4e12d81c0f953e54fcb10fb7ef
migueel15 <miguelangeldorado10@gmail.com>
2026-05-24 18:00:56 +0200

    update reloadHyprland script

--- quickshell/common/components/VolumeOSD.qml
+++ quickshell/common/components/VolumeOSD.qml
@@ -3,6 +3,7 @@ import QtQuick.Layouts
 import Quickshell
 import Quickshell.Services.Pipewire
 import Quickshell.Widgets
+import Quickshell.Wayland
 import qs.common
 
 Scope {
--- quickshell/utils/reloadHyprland.sh
+++ quickshell/utils/reloadHyprland.sh
@@ -8,5 +8,4 @@ y="$(jq -r '.y' <<< "$json")"
 hyprctl reload
 hyprctl dispatch "hl.dsp.cursor.move({x=$x, y=$y})"
 
-pkill quickshell 
-hyprctl dispatch 'hl.dsp.exec_cmd("quickshell")'
+/home/miguel/repos/SendaDesktopManager/core/dist/senda shell restart >/tmp/senda-restart.log 2>&1 &
